0d7df3c1a22116b5d1499eab18633f664e393b34
haneenawadallah <haneen.awadallah.995@gmail.com>
2020-04-30 23:14:47 +0300

    js added

--- javascript.js
+++ javascript.js
@@ -1,14 +1,8 @@
 'use strict';
 alert("Welcome to our webpage!!!");
 
-var str=window.prompt(" Enter your name ");
+var str = window.prompt(" Enter your name ");
 // document.write(" Welcome " + str );
 var welcome = "Welcome " + str;
 document.getElementById("haneen").innerHTML = welcome;
 
-var confirm1 = confirm("are you interested in traveling?")
-if(!confirm1){
-alert("i hope you get better");
-}else{
-    alert("thats great");
-}
