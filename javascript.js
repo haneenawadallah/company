daf9dd053c76dd44c873d241c9ef2ed96ad9efe1
haneenawadallah <haneen.awadallah.995@gmail.com>
2020-05-02 16:36:32 +0300

    edited size

--- javascript.js
+++ javascript.js
@@ -17,26 +17,26 @@ if (confirm("Press a button!")) {
     document.getElementById("here").style.color = "pink";
   }
 
-  function userChoice(){
-      var userChoice = prompt('where would you like to travel? New York City or London')
-      var item = '';
-      while (userChoice !== 'New York City' && userChoice !== 'London'){
-          userChoice = prompt('where would you like to travel? New York City or London')
-      }
-      var travelersNum = ('how many people wants to travel there?');
-      for (var i =0; i<travelersNum; i++){
-      if(userChoice === 'New York City'){
-          item = item + 'img src="https://i.pinimg.com/originals/d0/c3/9c/d0c39c06a0b0f590ea7037a9d8b4abcb.jpg"';
+ // function userChoice(){
+     // var userChoice = prompt('where would you like to travel? New York City or London')
+     // var item = '';
+   //   while (userChoice !== 'New York City' && userChoice !== 'London'){
+       //   userChoice = prompt('where would you like to travel? New York City or London')
+     // }
+     // var travelersNum = ('how many people wants to travel there?');
+      //for (var i =0; i<travelersNum; i++){
+      //if(userChoice === 'New York City'){
+         // item = item + 'img src="https://i.pinimg.com/originals/d0/c3/9c/d0c39c06a0b0f590ea7037a9d8b4abcb.jpg"';
          // document.write('<p><img src="https://treknova.com/travel-advice/wp-content/uploads/2019/08/nyc-1.jpg"</p>');
     
-      }else if (userChoice === 'London') {
-          item = item + '<img src="https://i.etsystatic.com/19870296/r/il/f7a3f7/1998370004/il_570xN.1998370004_m2d2.jpg"';
+     // }else if (userChoice === 'London') {
+         // item = item + '<img src="https://i.etsystatic.com/19870296/r/il/f7a3f7/1998370004/il_570xN.1998370004_m2d2.jpg"';
          // document.write('<p><img src ="https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg"</p>');
         
-      }
+      //}
 
-      }
-      return item;
-    }
+     // }
+     // return item;
+    //}
   
 
